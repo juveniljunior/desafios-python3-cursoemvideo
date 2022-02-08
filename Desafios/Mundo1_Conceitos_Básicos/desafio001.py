4bc21102a69188817be914e5772e9ae6635c2df4
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 18:37:28 -0300

    Respondendo ao Usuário

--- Desafios/Mundo1_Conceitos_Básicos/desafio001.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio001.py
@@ -1,4 +1,7 @@
-# DEIXANDO TUDO PRONTO
+"""
+Exercício Python 002:
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+"""
 
 nome = input('\033[31mQual seu nome? ')
 print(f'Olá! Prazer em te conhecer, \033[1;35m{nome}!\033[m\n'
