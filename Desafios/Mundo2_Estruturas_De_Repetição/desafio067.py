f277e10a0310c8ea5c05eea37c0589cccb7b0c40
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 08:54:13 -0300

    Tabuada v3.0

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio067.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio067.py
@@ -1,4 +1,10 @@
 # TABUADA v3.0
+"""
+Exercício Python 067:
+Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado
+pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+"""
+
 contador = num = 0
 while True:
     num = int(input('Você quer ver a tabuada de que número? '))
