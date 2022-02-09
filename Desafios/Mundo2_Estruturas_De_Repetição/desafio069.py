7703a359723ac730e1e3091c8aa517a36f8c755c
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 08:56:30 -0300

    Análise de dados do grupo

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio069.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio069.py
@@ -1,4 +1,12 @@
-# ANÁLISE DE DADOS DO GRUPO
+"""
+Exercício Python 069:
+Crie um programa que leia a idade e o sexo de várias pessoas. A
+cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:
+A) quantas pessoas tem mais de 18 anos.
+B) quantos homens foram cadastrados.
+C) quantas mulheres tem menos de 20 anos.
+"""
+
 maioresdeidade = homens = novinhas = 0
 while True:
     print(f'{"="*40}\n{" CADASTRE UM PESSOA ":-^40}\n{"="*40}')
