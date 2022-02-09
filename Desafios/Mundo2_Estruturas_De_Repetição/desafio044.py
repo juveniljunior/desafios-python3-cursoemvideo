c9e0d3d648606a3b38b2d88aecfe00c72a3d6472
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:40:11 -0300

    Gerenciador de Pagamentos

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio044.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio044.py
@@ -1,4 +1,11 @@
-# GERENCIADOR DE PAGAMENTOS
+"""
+Exercício Python 044:
+Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
+- à vista dinheiro/cheque: 10% de desconto
+- à vista no cartão: 5% de desconto
+- em até 2x no cartão: preço formal
+- 3x ou mais no cartão: 20% de juros
+"""
 
 print('--'*20)
 print(f'{" LOJAS JUNIOR ":=^40}')
