81e362b6ce21700a1ba7d531badb4b7995d77d23
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:30:37 -0300

    Aumentos múltiplos

--- Desafios/Mundo1_Conceitos_Básicos/desafio034.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio034.py
@@ -1,3 +1,10 @@
+"""
+Exercício Python 034:
+Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores
+a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+"""
+
 sal = float(input('\033[1;36mDigite o seu salário(R$): '))
 aum_sal = sal + (sal*10/100)
 aum_sal2 = sal + (sal*15/100)
