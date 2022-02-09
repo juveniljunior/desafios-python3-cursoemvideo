2e9724d03194881117f8ba1f98dd2df67c0a78e8
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:29:33 -0300

    Maior e menor valores

--- Desafios/Mundo1_Conceitos_Básicos/desafio033.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio033.py
@@ -1,3 +1,8 @@
+"""
+Exercício Python 033:
+Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+"""
+
 """RACIOCÍNIO LOGÍCO"""
 a = int(input('\033[34m1º VALOR: '))
 b = int(input('\033[33m2º VALOR: '))
