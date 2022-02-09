926be924ffc1dd294d7dbc11307038b0387f82fc
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:05:40 -0300

    Lista de Preços com Tupla

--- Desafios/Mundo3_Estruturas_Compostas/desafio076.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio076.py
@@ -1,3 +1,9 @@
+"""
+Exercício Python 076:
+Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na
+sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+"""
+
 """print(f'{"-"*40}\n{"LISTAGEM DE PREÇOS":^40}\n{"-"*40}')
 lista = ('Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25, 'Transferidor', 4.20,
                           'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livros', 34.90)
@@ -10,6 +16,7 @@ print(f'{lista[0]:.<30}R${lista[1]:>8.2f}\n'
       f'{lista[12]:.<30}R${lista[13]:>8.2f}\n'
       f'{lista[14]:.<30}R${lista[15]:>8.2f}\n'
       f'{lista[16]:.<30}R${lista[17]:>8.2f}')"""
+
 lista = ('Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25, 'Transferidor', 4.20, 'Compasso', 9.99,
          'Mochila', 120.32, 'Canetas', 22.30, 'Livros', 34.90)
 print(f'{"-"*40}\n{"LISTAGEM DE PREÇOS":^40}\n{"-"*40}')
