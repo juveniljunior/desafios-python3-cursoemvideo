8a79a7147db2d14ee90c203f60e63e4124624169
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:08:11 -0300

    Quebrando um número

--- Desafios/Mundo1_Conceitos_Básicos/desafio016.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio016.py
@@ -1,3 +1,9 @@
+"""
+Exercício Python 016:
+Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção
+Inteira.
+"""
+
 ''' from math import trunc
 num = float(input('Digite um número: '))
 nint = trunc(num)
