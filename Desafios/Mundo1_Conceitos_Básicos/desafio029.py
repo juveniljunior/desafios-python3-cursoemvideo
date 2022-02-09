99ca666029930b16843d3efdbd840273abf8e9f9
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:23:25 -0300

    Radar eletrônico

--- Desafios/Mundo1_Conceitos_Básicos/desafio029.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio029.py
@@ -1,3 +1,8 @@
+"""Exercício Python 029:
+Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,
+mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+"""
+
 vel = float(input('\033[36mQual foi a velocidade do carro(Km/h)? '))
 mul = (vel - 80.0) * 7.0
 if vel > 80.0:
