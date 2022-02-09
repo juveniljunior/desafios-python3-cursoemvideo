44258890248555365e602eac3e3538ccbba761f3
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:01:59 -0300

    Tuplas com Times de Futebol

--- Desafios/Mundo3_Estruturas_Compostas/desafio073.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio073.py
@@ -1,4 +1,12 @@
-# TUPLAS COM TIMES DE FUTEBOL
+"""
+Exercício Python 073:
+Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
+a) Os 5 primeiros times.
+b) Os últimos 4 colocados.
+c) Times em ordem alfabética.
+d) Em que posição está o time da Chapecoense.
+"""
+
 primeiros20br = ('Atlético-MG', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians', 'Bragantino', 'Fluminense',
                  'América-MG', 'Atlético-GO', 'Santos', 'Ceará', 'Internacional', 'São Paulo', 'Athletico-PR', 'Cuiabá',
                  'Juventude', 'Grêmio', 'Bahia', 'Sport', 'Chapecoense')
