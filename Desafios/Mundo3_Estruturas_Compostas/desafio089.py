371464e945ff1eb2cfc6fe999b9d992d3997eb7c
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:20:34 -0300

    Boletim com listas compostas

--- Desafios/Mundo3_Estruturas_Compostas/desafio089.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio089.py
@@ -1,4 +1,9 @@
-# BOLETIM COM LISTAS COMPOSTAS
+"""
+Exercício Python 089:
+Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta.
+No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
+"""
+
 from time import sleep
 lista = list()
 aluno = list()
