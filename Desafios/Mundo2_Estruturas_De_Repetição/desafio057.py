5415a3f34532ba5e515bf7d400aed9710aa65e95
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:58:18 -0300

    Validação de Dados

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio057.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio057.py
@@ -1,4 +1,8 @@
-# VALIDAÇÃO DE DADOS
+"""Exercício Python 057:
+Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso
+esteja errado, peça a digitação novamente até ter um valor correto.
+"""
+
 sexo = str(input('Qual seu sexo? M ou F? ')).upper().strip()[0]
 while sexo not in 'FM':
     sexo = str(input('Dados inválidos! Qual seu sexo? M ou F? ')).upper().strip()[0]
