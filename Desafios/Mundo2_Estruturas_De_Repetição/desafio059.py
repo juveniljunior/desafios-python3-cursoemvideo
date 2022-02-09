a00205e820859f73294d91417dbcd6d88bcbb941
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 23:00:55 -0300

    Criando um Menu de Opções

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio059.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio059.py
@@ -1,4 +1,14 @@
-# CRIANDO UM MENU DE OPÇÕES
+"""
+Exercício Python 059:
+Crie um programa que leia dois valores e mostre um menu na tela:
+[ 1 ] somar
+[ 2 ] multiplicar
+[ 3 ] maior
+[ 4 ] novos números
+[ 5 ] sair do programa
+Seu programa deverá realizar a operação solicitada em cada caso.
+"""
+
 from time import sleep
 opcao = 0
 n1 = int(input('Digite um valor: '))
