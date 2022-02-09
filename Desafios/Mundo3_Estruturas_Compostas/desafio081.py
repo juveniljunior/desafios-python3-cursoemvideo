f69a972b86f81967986314dd20353ecb144b8772
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:10:54 -0300

    Extraindo dados de uma Lista

--- Desafios/Mundo3_Estruturas_Compostas/desafio081.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio081.py
@@ -1,4 +1,11 @@
-# EXTRAINDO DADOS EM UMA LISTA
+"""
+Exercício Python 081:
+Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
+A) Quantos números foram digitados.
+B) A lista de valores, ordenada de forma decrescente.
+C) Se o valor 5 foi digitado e está ou não na lista.
+"""
+
 valores = []
 while True:
     valores.append(int(input('Digite um valor: ')))
