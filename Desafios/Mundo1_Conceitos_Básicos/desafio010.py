a6c84b273a23f58984232517961856f92f791fda
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:02:02 -0300

    Conversor de Moedas

--- Desafios/Mundo1_Conceitos_Básicos/desafio010.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio010.py
@@ -1,4 +1,7 @@
-# CONVERSOR DE MOEDAS
+"""
+Exercício Python 010:
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+"""
 
 n = float(input('\33[1;31mQuanto você tem na carteira? '))
 print(f'Você pode comprar \33[1;34m{n/3.27:.2f} \33[m \33[1;31mdólares!')
