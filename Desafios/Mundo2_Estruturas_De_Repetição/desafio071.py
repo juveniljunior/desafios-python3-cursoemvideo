693a2c911ec2fc855cf17ed84c4b9e928a07f031
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 08:59:16 -0300

    Simulador de Caixa Eletrônico

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio071.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio071.py
@@ -1,4 +1,10 @@
 # SIMULADOR DE CAIXA ELETRÔNICO
+"""
+Exercício Python 071:
+Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
+OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+"""
+
 print(f'{"="*30:^30}\n{" BANCO JUNIORBANK ":-^30}\n{"="*30:^30}')
 valor = int(input('Qual valor você quer sacar?R$ '))
 while True:
