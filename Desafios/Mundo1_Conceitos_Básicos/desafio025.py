93c4d73860e9fb738ffe297de701bb512416c0cc
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:19:05 -0300

    Procurando uma string dentro de outra

--- Desafios/Mundo1_Conceitos_Básicos/desafio025.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio025.py
@@ -1,4 +1,6 @@
-# PROCURANDO UMA STRING DENTRO DE OUTRA
+"""
+Exercício Python 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+"""
 
 nome = input('\33[31mQual seu nome completo? ').strip().upper()
 print(f'\33[32mVocê contém "Silva" no nome? \nResposta: \33[36;1m{"SILVA" in nome.split()}')
