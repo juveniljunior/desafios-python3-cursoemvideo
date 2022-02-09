4d2e9467dc8e814a3c0be3ba935821a0eef2a0df
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:43:13 -0300

    Contagem de pares

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio047.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio047.py
@@ -1,4 +1,7 @@
-# CONTAGEM DE PARES
+"""
+Exercício Python 047:
+Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+"""
 
 print('Os números pares de 1 a 50 são: ')
 for c in range(1, 51, 2):
