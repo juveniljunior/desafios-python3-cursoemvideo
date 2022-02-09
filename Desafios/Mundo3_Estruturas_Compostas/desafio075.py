ebfe647b37b4135b314daf7a9c2e11773d4eb966
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:04:22 -0300

    Análise de dados em uma Tupla

--- Desafios/Mundo3_Estruturas_Compostas/desafio075.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio075.py
@@ -1,4 +1,11 @@
-# ANÁLISE DE DADOS EM UMA TUPLA
+"""
+Exercício Python 075:
+Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+
+A) Quantas vezes apareceu o valor 9.
+B) Em que posição foi digitado o primeiro valor 3.
+C) Quais foram os números pares.
+"""
 numeros = (int(input('Digite um número: ')), int(input('Digite outro número: ')), int(input('Digite mais um número: ')),
            int(input('Digite só mais um número: ')),)
 print(f'Os valores inseridos foram: {numeros}.\nO valor 9 apareceu {numeros.count(9)} vez(es).')
