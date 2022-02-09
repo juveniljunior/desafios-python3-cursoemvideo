2c0f6c66fc15d5a8cfeadbd6da96ae2a877e7a08
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:56:48 -0300

    Exercitando módulos em Python

--- Desafios/Mundo3_Estruturas_Compostas/desafio107_modulo/desafio107-aula22.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio107_modulo/desafio107-aula22.py
@@ -10,3 +10,4 @@ print(f'A metade de {n} é {moeda.metade(n)}')
 print(f'O dobro de {n} é {moeda.dobro(n)}')
 print(f'Aumentando 10%, temos {moeda.aumentar(n, 10)}')
 print(f'Reduzindo 13%, temos {moeda.diminuir(n, 13)}')
+
