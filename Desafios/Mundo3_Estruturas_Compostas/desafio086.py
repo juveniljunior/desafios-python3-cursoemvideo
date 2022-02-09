5c96b10f2450471499bdb58a67f5327b8f436488
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:16:41 -0300

    Matriz em Python

--- Desafios/Mundo3_Estruturas_Compostas/desafio086.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio086.py
@@ -1,4 +1,9 @@
-# MATRIZ EM PYTHON
+"""
+Exercício Python 086:
+Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a
+matriz na tela, com a formatação correta.
+"""
+
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for l in range(0, 3):
     for c in range(0, 3):
