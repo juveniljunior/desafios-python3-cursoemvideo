d29b1951e55cbd820b0eb8c37bc54f17894219cc
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:24:41 -0300

    Par ou Ímpar?

--- Desafios/Mundo1_Conceitos_Básicos/desafio030.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio030.py
@@ -1,3 +1,7 @@
+"""
+Exercício Python 030:
+Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+"""
 num = int(input('\033[33mDigte um número: \033[m'))
 par = num % 2
 if par == 0:
