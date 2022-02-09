92dd00cba4578493bd9bbbe58e08d3826ac21122
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:10:13 -0300

    Seno, Cosseno e Tangente

--- Desafios/Mundo1_Conceitos_Básicos/desafio018.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio018.py
@@ -1,4 +1,8 @@
-# SENO, COSSENO E TANGENTE DE ÃNGULO
+"""
+Exercício Python 018:
+Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+"""
+
 '''from math import sin, cos, tan, degrees, radians
 ang = float(input('Digite o ângulo do triângulo: '))
 s = sin(radians(ang))
