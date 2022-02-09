0da63515502505c6612a78728887b4ada124a1a5
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:59:34 -0300

    Jogo da Adivinhação v2.0

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio058.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio058.py
@@ -1,4 +1,10 @@
 # JOGO DA ADVINHAÇÃO 2.0
+"""Exercício Python 058:
+Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10. Só
+que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para
+vencer.
+"""
+
 from random import randint
 comp = randint(0, 10)
 usu = 11
