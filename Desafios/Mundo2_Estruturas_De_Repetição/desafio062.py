a9e52c27906890cd22180c46741a1d4bec338906
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 08:48:37 -0300

    Super Progressão Aritmética v3.0

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio062.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio062.py
@@ -1,4 +1,8 @@
 # SUPER PROGRESSÃO ARITMÉTICA v3.0
+"""Exercício Python 062:
+Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O
+programa encerrará quando ele disser que quer mostrar 0 termos. """
+
 contador = 0
 novocontador = 1
 nc = 1
