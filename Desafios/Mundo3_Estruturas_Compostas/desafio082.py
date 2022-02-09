6f5833923996e480c4309f8687b7212de0a9cf71
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:11:54 -0300

    Dividindo valores em várias listas

--- Desafios/Mundo3_Estruturas_Compostas/desafio082.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio082.py
@@ -1,4 +1,9 @@
-# DIVIDINDO VALORES EM UMA LISTA
+"""
+Exercício Python 082:
+Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas
+extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+"""
+
 numeros = numerospares = numerosimpares = []
 while True:
     numeros.append(int(input('Digite um número: ')))
