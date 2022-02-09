17e6f5af9e3c6bf35cb9635f829da72b5b629780
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:13:13 -0300

    Validando expressões matemáticas

--- Desafios/Mundo3_Estruturas_Compostas/desafio083.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio083.py
@@ -1,4 +1,9 @@
-# VALIDANDO EXPRESSÕES MATEMÁTICAS
+"""
+Exercício Python 083:
+Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo
+deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+"""
+
 expressao = [str(input('Digite a expressão: '))]
 pilha = 0
 for elementos in expressao:
