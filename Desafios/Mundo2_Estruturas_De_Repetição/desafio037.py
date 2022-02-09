c72373420032b63810445022b69f4c965e50c035
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:34:26 -0300

    Conversor de Bases Numéricas

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio037.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio037.py
@@ -1,4 +1,8 @@
-# CONVERSOR DE BASES NUMÉRICAS
+"""
+Exercício Python 037:
+Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário
+escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+"""
 
 numero = int(input('Informe um número inteiro: '))
 base = int(input(f'\33[35;1m[1] Binário\n'
