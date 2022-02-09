042c0b48adde23150a8e097fa4519d6d50bb99f3
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 23:02:09 -0300

    Cálculo do Fatorial

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio060.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio060.py
@@ -1,4 +1,9 @@
-# CÁLCULO DO FATORIAL
+"""
+Exercício Python 060:
+Faça um programa que leia um número qualquer e mostre o seu fatorial.
+
+Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
+"""
 """from time import sleep
 n = int(input('Digite um número e veja seu fatorial: '))
 res = n
