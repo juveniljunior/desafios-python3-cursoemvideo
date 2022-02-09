de0124d05a92f89ab19c650654e039143fa2b51a
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:42:11 -0300

    Contagem regressiva

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio046.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio046.py
@@ -1,4 +1,9 @@
 # CONTAGEM REGRESSIVA
+"""
+Exercício Python 046:
+Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+"""
 import emoji
 from time import sleep
 for c in range(10, -1,  -1):
