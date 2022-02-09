ae6d4c7cf220fcc9e7b6829f75abcc2e75b7d637
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:35:18 -0300

    Comparando números

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio038.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio038.py
@@ -1,4 +1,10 @@
-# COMPARANDO NÚMEROS
+"""
+Exercício Python 038:
+Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+- O primeiro valor é maior
+- O segundo valor é maior
+- Não existe valor maior, os dois são iguais
+"""
 
 a = int(input('\33[31;1mPrimeiro valor: '))
 b = int(input('\33[34;1mSegundo valor: '))
