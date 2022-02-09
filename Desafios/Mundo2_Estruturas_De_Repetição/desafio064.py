2d906f1aa700451ea2ddcb14d40e65bd959b604a
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 08:50:53 -0300

    Tratando vários valores v1.0

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio064.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio064.py
@@ -1,4 +1,8 @@
-# TRATANDO VÁRIOS VALORES
+"""Exercício Python 064: Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar
+quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e
+qual foi a soma entre eles (desconsiderando o flag).
+"""
+
 c = n = s = 0
 while n != 999:
     n = int(input('Digite um número (ou 999 para parar): '))
