547fb42abf90917aa779992c64481a0d28f2e713
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 23:03:22 -0300

    Progressão Aritmética v2.0

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio061.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio061.py
@@ -1,4 +1,10 @@
 # PROGRESSÃO ARITMÉTICA v2.0
+"""
+Exercício Python 061:
+Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos
+da progressão usando a estrutura while.
+"""
+
 contador = 0
 primeirotermo = int(input('1º TERMO DA PA: '))
 razao = int(input('RAZÃO DA PA: '))
