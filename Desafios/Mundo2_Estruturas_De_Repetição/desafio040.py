1dec3acf879ead266b6c7fb468c8ee5b5b9f7593
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:37:02 -0300

    Aquele clássico da Média

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio040.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio040.py
@@ -1,4 +1,10 @@
-# SITUAÇÃO FINAL DE UM ALUNO
+"""
+Exercício Python 040:
+Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+- Média abaixo de 5.0: REPROVADO
+- Média entre 5.0 e 6.9: RECUPERAÇÃO
+- Média 7.0 ou superior: APROVADO
+"""
 
 n1 = float(input('Primeira nota: '))
 n2 = float(input('Segunda nota: '))
