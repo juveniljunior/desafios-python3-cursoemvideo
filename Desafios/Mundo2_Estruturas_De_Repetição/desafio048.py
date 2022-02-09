0b39bd469b26aedada56df9b08a3247e1e4130e4
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:44:35 -0300

    Soma ímpares múltiplos de três

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio048.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio048.py
@@ -1,4 +1,6 @@
-# SOMA IMPÁRES MÚLTIPLOS DE TRÊS
+"""Exercício Python 048:
+Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que
+se encontram no intervalo de 1 até 500. """
 s = 0
 np = 0
 for c in range(1, 501, 2):
