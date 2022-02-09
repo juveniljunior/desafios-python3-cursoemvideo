41019e1fe7c0691e27741df4c933e66ea5fb1777
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:55:48 -0300

    Analisador completo

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio056.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio056.py
@@ -1,4 +1,10 @@
-# ANALISADOR COMPLETO II
+"""
+Exercício Python 056:
+Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do
+grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+
+"""
+
 velho = ''
 idades = []
 idadedoshomens = []
