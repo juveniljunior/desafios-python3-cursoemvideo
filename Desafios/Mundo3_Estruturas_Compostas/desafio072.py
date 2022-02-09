f9c2e1cd48a31be679ee250460626e9251c06338
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:01:04 -0300

    Número por Extenso

--- Desafios/Mundo3_Estruturas_Compostas/desafio072.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio072.py
@@ -1,4 +1,8 @@
-# NÚMERO POR EXTENSO
+"""
+Exercício Python 072:
+Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa
+deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+"""
 tupla0a20 = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
              'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
 while True:
