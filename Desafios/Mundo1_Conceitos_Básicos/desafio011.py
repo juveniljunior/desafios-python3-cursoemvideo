df5be7e30b431edd7435b90db7062c43fdcff3ff
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:03:06 -0300

    Pintando Parede

--- Desafios/Mundo1_Conceitos_Básicos/desafio011.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio011.py
@@ -1,3 +1,9 @@
+"""
+Exercício Python 011:
+Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e
+a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+"""
+
 a = float(input('\33[1;31mDigite o valor da altura: '))
 l = float(input('\33[1;33mDigite o valor da largura: '))
 area = a*l
