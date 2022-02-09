f7e7f5c45613f116e70dcdd0e3b0656b66b13297
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 21:49:14 -0300

    Antecessor e Sucessor

--- Desafios/Mundo1_Conceitos_Básicos/desafio005.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio005.py
@@ -1,4 +1,8 @@
-# ANTECESSOR E SUCESSOR
+"""
+Exercício Python 005:
+Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+"""
 
 num = int(input('\33[36mDigite um número: \33[m'))
 print(f'O antecessor de \033[1;4;32m{num}\33[m é \033[1;4;32m{num-1}\33[m e o seu sucessor é \033[1;4;32m{num+1}')
