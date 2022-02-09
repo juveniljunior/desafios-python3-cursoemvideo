239e5ad21cd24e9d06213783c41f015c75139047
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:07:57 -0300

    Maior e Menor valores na Lista

--- Desafios/Mundo3_Estruturas_Compostas/desafio078.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio078.py
@@ -1,4 +1,10 @@
-#  MAIOR E MENOR VALORES DE UMA LISTA
+"""
+Exercício Python 078:
+Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor
+valor digitado e as suas respectivas posições na lista.
+
+"""
+
 lista = []
 mai = men = 0
 for c in range(0, 5):
