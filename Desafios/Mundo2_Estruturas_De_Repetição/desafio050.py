9a9d599529009ada956b80f4f0c498019be8ae28
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:46:35 -0300

    Soma dos pares

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio050.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio050.py
@@ -1,4 +1,9 @@
-# SOMA DE PARES
+"""
+Exercício Python 050:
+Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem
+pares. Se o valor digitado for ímpar, desconsidere-o.
+"""
+
 s = 0
 cont = 0
 for c in range(1, 7):
