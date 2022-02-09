15bba87093ed73f6c98b75e7d05314506ce4dead
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:07:11 -0300

    Aluguel de Carros

--- Desafios/Mundo1_Conceitos_Básicos/desafio015.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio015.py
@@ -1,4 +1,9 @@
-# ALUGUEL DE UM CARRO
+"""
+Exercício Python 015:
+Escreva um programa que pergunte a quantidade de
+Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+
+"""
 
 aluguel = float(input('\33[1;37mQuantos dias você usou o carro? '))
 km = float(input('\33[1;97mQuantos kilômetros você percorreu? '))
