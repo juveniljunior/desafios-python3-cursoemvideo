e4796a0f12744edb7e866df0432b77312026749a
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:14:13 -0300

    Lista composta e análise de dados

--- Desafios/Mundo3_Estruturas_Compostas/desafio084.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio084.py
@@ -1,4 +1,11 @@
-# LISTA COMPOSTA E ANÁLISE DE DADOS
+"""
+Exercício Python 084:
+Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:
+A) Quantas pessoas foram cadastradas.
+B) Uma listagem com as pessoas mais pesadas.
+C) Uma listagem com as pessoas mais leves.
+"""
+
 galera = list()
 dados = list()
 maior = menor = 0
