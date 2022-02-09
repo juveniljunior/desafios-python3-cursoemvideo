ebf1004de02fa79f3084e9553f2ff1c2d1a05de2
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:45:22 -0300

    Tabuada v.2.0

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio049.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio049.py
@@ -1,4 +1,8 @@
-# TABUADA v2.0
+"""
+Exercício Python 049:
+Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora
+utilizando um laço for.
+"""
 
 n = int(input('Digite um número para ver sua tabuada: '))
 print('='*12)
