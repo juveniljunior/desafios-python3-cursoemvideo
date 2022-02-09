c5dab7fc47d0b71de0bdcb0b6326fbd15b45c001
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:52:16 -0300

    Grupo da Maioridade

--- Desafios/Mundo2_Estruturas_De_Repetição/desafio054.py
+++ Desafios/Mundo2_Estruturas_De_Repetição/desafio054.py
@@ -1,4 +1,8 @@
-# GRUPO DA MAIORIDADE
+"""Exercício Python 054:
+Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas
+pessoas ainda não atingiram a maioridade e quantas já são maiores.
+"""
+
 from datetime import date
 s = 0
 s2 = 0
