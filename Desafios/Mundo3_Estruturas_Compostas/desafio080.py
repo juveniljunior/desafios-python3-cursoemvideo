a1e7856f92fe74ae5de3c2a909781c85e48e2439
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:09:49 -0300

    Lista ordenada sem repetições

--- Desafios/Mundo3_Estruturas_Compostas/desafio080.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio080.py
@@ -1,4 +1,9 @@
-# LISTA ORDENADA SEM REPETIÇÕES
+"""
+Exercício Python 080:
+Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta
+de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+"""
+
 lista = []
 for c in range(0, 5):
     num = int(input('Digite um valor: '))
