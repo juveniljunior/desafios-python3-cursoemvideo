bdd7ade054c684c825037fecb558dcc2d1e43763
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:18:05 -0300

    Verificando as primeiras letras de um texto

--- Desafios/Mundo1_Conceitos_Básicos/desafio024.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio024.py
@@ -1,4 +1,6 @@
-# VERIFICANDO AS PRIMEIRAS LETRAS DE UM TEXTO
+"""
+Exercício Python 024: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+"""
 
 city = str(input('\33[32mEm que cidade você mora? ')).strip().capitalize()
 print(f'A sua cidade começa com "Santo"? \nResposta: \33[1;4;34m{city.startswith("Santo")}')
