a6c9514a9a01311dc9cd434b7bd562038ccc3501
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:03:19 -0300

    Maior e menor valores em Tupla

--- Desafios/Mundo3_Estruturas_Compostas/desafio074.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio074.py
@@ -1,3 +1,9 @@
+"""
+Exercício Python 074:
+Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de
+números gerados e também indique o menor e o maior valor que estão na tupla.
+
+"""
 from random import randint
 numerossorteados = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
 print('Os valores sorteados foram:', end=' ')
