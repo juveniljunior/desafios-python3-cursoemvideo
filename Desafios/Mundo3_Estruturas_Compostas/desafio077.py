2476020c42504ceed78d0a777e68d64b0b3c2c99
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:06:44 -0300

    Contando vogais em Tupla

--- Desafios/Mundo3_Estruturas_Compostas/desafio077.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio077.py
@@ -1,3 +1,9 @@
+"""
+Exercício Python 077:
+Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você
+deve mostrar, para cada palavra, quais são as suas vogais.
+"""
+
 palavras = ('item', 'gratuito', 'proibido', 'rubrica', 'recorde', 'rudico')  # Tuplas das palavras a serem analisadas
 for p in palavras:  # Para cada palavra da tupla
     print(f'\nNa palavra {p.upper()} temos ', end='')  # Palavra a ser analisada
