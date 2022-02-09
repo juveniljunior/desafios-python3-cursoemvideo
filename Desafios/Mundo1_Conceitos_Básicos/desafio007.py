a051a1199a3fa5ebc32fc8956734c6bf7a2973cc
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 21:51:30 -0300

    Média Aritmética

--- Desafios/Mundo1_Conceitos_Básicos/desafio007.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio007.py
@@ -1,4 +1,7 @@
-# ALGORITMO QUE LÊ DUAS NOTAS E MOSTRA A MÉDIA DELAS
+"""
+Exercício Python 007:
+Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+"""
 
 n1 = float(input("\33[36mDigite a primeira nota: "))
 n2 = float(input('\33[37mDigite a segunda nota: '))
