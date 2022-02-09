38de463fc4fade4e27f5591376477b3d95f45c33
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:19:57 -0300

    Primeira e última ocorrência de uma string

--- Desafios/Mundo1_Conceitos_Básicos/desafio026.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio026.py
@@ -1,4 +1,6 @@
-# PRIMEIRA E ÚLTIMA OCORRÊNCIA DE UMA STRING
+"""Exercício Python 026:
+Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A",
+em que posição ela aparece a primeira vez e em que posição ela aparece a última vez """
 
 phrase = str(input('\33[35mDigite uma frase: ')).strip().upper()
 print(f'Nesta frase a letra "A" aparece \33[33;1;4m{phrase.count("A")} \33[35mvezes.\n'
