e19eeac257396dc4374195d1996484b5bdbfd298
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-08 22:04:15 -0300

    Calculando Descontos

--- Desafios/Mundo1_Conceitos_Básicos/desafio012.py
+++ Desafios/Mundo1_Conceitos_Básicos/desafio012.py
@@ -1,4 +1,8 @@
-# DESCONTO DE UM PRODUTO
+"""
+Exercício Python 012:
+Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de
+desconto.
+"""
 
 preço = float(input("\33[33mDigite o preço do produto(R$): "))
 novo = preço - (preço*5/100)
