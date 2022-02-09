57583274ee52065cae38e3fe7b99110c9dd2f743
Juvenil Junior <juvenildejesus402@gmail.com>
2022-02-09 09:17:26 -0300

    Mais sobre Matriz em Python

--- Desafios/Mundo3_Estruturas_Compostas/desafio087.py
+++ Desafios/Mundo3_Estruturas_Compostas/desafio087.py
@@ -1,4 +1,11 @@
-# MAIS SOBRE MATRIZ EM PYTHON
+"""
+Exercício Python 087:
+Aprimore o desafio anterior, mostrando no final:
+A) A soma de todos os valores pares digitados.
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha.
+"""
+
 matriz = list()
 linha = list()
 somap = maiorlin2 = 0
